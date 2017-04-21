de871249158a978b3a66b15ed087d6f506aef326
Kevin Hinds <khinds10.work@gmail.com>
2017-04-21 16:09:17 -0400

    added temp-check script for remote logging conditions

--- settings.py
+++ settings.py
@@ -6,6 +6,9 @@
 weatherAPIURL = 'https://api.forecast.io/forecast/'
 weatherAPIKey = 'YOUR API KEY FOR FORECAST.IO'
 
+# optional for running the remote temp/humidity logger
+deviceLoggerAPI = 'mydevicelogger.com'
+
 # search google to get the Latitude/Longitude for your home location
 latitude = 42.4152778
 longitude = -71.1569444
